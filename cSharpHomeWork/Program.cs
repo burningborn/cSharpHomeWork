5b4ac97d62d0ed69280780860de1e689ef52eaf9
burningborn <evgenyborn@gmail.com>
2021-11-25 11:28:48 +0400

    MatrixAction

--- cSharpHomeWork/Program.cs
+++ cSharpHomeWork/Program.cs
@@ -8,9 +8,98 @@ namespace cSharpHomeWork
 {
     class Program
     {
+        static void printArr(int [,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                   // Console.Write($"{arr[i, j]} ");
+                    Console.Write("Element ({0},{1}) = {2} ", i, j, arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static int Average(int[,] arr)
+        {
+            int Average = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Average += arr[i, j];
+                }
+            }
+            Average /= arr.Length;
+            return Average;
+        }
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow World");
+
+            int[,] arr1, arr2, resArr  = null; // создали две матрицы
+
+            Console.Write("Задайте количество столбцов матрицы: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Задайте количество строк матрицы: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            arr1 = new int[m, n]; // выделение памяти под первую матрицу
+            arr2 = new int[m, n]; // выделение памяти под первую матрицу
+            resArr = new int[m, n]; // выделение памяти под первую матрицу
+
+            // инициализация элементов матриц
+            Random rand = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                for(int j=0;j<n; j++)
+                {
+                    arr1[i, j] = rand.Next(-99, 99);
+                    arr2[i, j] = rand.Next(-99, 99);
+                }
+
+            }
+            //вывод матрицы 1 на экран
+            Console.WriteLine("Матрица_#1: ");
+            printArr(arr1);
+            // вывод матрицы 2 на экран
+            Console.WriteLine("Матрица_#2: ");
+            printArr(arr2);
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    resArr[i, j] = arr1[i, j] + arr2[i, j];
+                }
+            }
+            Console.WriteLine("Сумма матриц: ");
+            printArr(resArr);
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    resArr[i, j] = arr1[i, j] * arr2[i, j];
+                }
+            }
+            Console.WriteLine("Произведение матриц: ");
+            printArr(resArr);
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    resArr[i, j] = arr1[i, j] - arr2[i, j];
+                }
+            }
+            Console.WriteLine("Разность матриц: ");
+            printArr(resArr);
+               
+            Console.WriteLine($"Среднее арифметическое матрицы arr1: {Average(arr1)}");
+            Console.WriteLine($"Среднее арифметическое матрицы arr2: {Average(arr2)}");
+            Console.WriteLine($"Среднее арифметическое матрицы resArr: {Average(resArr)}");                   
         }
     }
 }
