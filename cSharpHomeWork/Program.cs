a7e1726780cf10ed48b5079b84b91b7d9dd85a4f
burningborn <evgenyborn@gmail.com>
2021-11-25 19:05:59 +0400

    gradeBill

--- cSharpHomeWork/Program.cs
+++ cSharpHomeWork/Program.cs
@@ -8,6 +8,55 @@ namespace cSharpHomeWork
 {
     class Program
     {
+        static void gradeBill(int[,] arr, int n)
+        {
+            Console.WriteLine("\t Ведомость успеваемости ");
+            Console.Write("Math:        ");
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[0, i] != 0)
+                    Console.Write(arr[0, i] + " ");
+                else
+                    break;
+            }
+            Console.WriteLine();
+            Console.Write("Programming: ");
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[1, i] != 0)
+                    Console.Write(arr[1, i] + " ");
+                else
+                    break;
+            }
+            Console.WriteLine();
+            Console.Write("Philosophy:  ");
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[2, i] != 0)
+                    Console.Write(arr[2, i] + " ");
+                else
+                    break;
+            }
+            Console.WriteLine();
+            Console.Write("Physics:     ");
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[3, i] != 0)
+                    Console.Write(arr[3, i] + " ");
+                else
+                    break;
+            }
+            Console.WriteLine();
+            Console.Write("English:     ");
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[4, i] != 0)
+                    Console.Write(arr[4, i] + " ");
+                else
+                    break;
+            }
+            Console.WriteLine();
+        }
         static void printArr(int [,] arr)
         {
             for (int i = 0; i < arr.GetLength(0); i++)
@@ -15,7 +64,7 @@ namespace cSharpHomeWork
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
                    // Console.Write($"{arr[i, j]} ");
-                    Console.Write("Element ({0},{1}) = {2} ", i, j, arr[i, j]);
+                    Console.Write("Element ({0},{1}) = {2} ", i, j, arr[i,j]);
                 }
                 Console.WriteLine();
             }
@@ -34,72 +83,164 @@ namespace cSharpHomeWork
             Average /= arr.Length;
             return Average;
         }
+        static int[,] gradeIn(int[,] arr, int m)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (arr[m, i] == 0)
+                {
+                    bool key = true;
+                    do
+                    {
+                        Console.Write("\nВведите отметку (1-5): "); 
+                        int grade = Convert.ToInt32(Console.ReadLine());
+                        if (grade > 0 && grade < 6)
+                        { 
+                            arr[m, i] = grade;
+                            key = false; 
+                        }
+                        else
+                            Console.Write("\nПопробуйте ещё (1-5): ");
+                        //key = true;
+                    } while (key);
+                    break;
+                }
+            }
+            Console.WriteLine();
+            return arr;
+        }
+        static void action()
+        {
+            
+        }
+        static int themeAverage(int [,] arr, int m)
+        {
+            int average = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                if (arr[m, i] != 0)
+                    average += arr[m, i];
+                else if (i == 0)
+                {                 
+                    break;
+                }
+                else
+                {
+                    average /= i;
+                    break;
+                }
+            }
+            return average;
+        }
+        static int allAverage(int[,] arr)
+        {
+            int average = 0, devider = 0;
+            foreach (int elem in arr)
+            {
+                if (elem != 0)
+                {
+                    average += elem;
+                    devider++;
+                }
+                else
+                    continue;
+            }
+            average /= devider;
+            return average;
+        }
+        static int maxGrade(int[,] arr)
+        {
+            int max = 0;
+            foreach (int elem in arr)
+            {
+                if (elem > max)
+                {
+                    max = elem;                  
+                }
+                else
+                    continue;
+            }           
+            return max;
+        }
+        static int minGrade(int[,] arr)
+        {
+            int min = 5;
+            foreach (int elem in arr)
+            {
+                if (elem != 0)
+                {
+                    if (elem < min)
+                        min = elem;
+                }               
+                else
+                    continue;
+            }
+            return min;
+        }
 
-        
+        static int themes()
+        {
+            Console.Clear();
+            Console.WriteLine("\n\tВыберите предмет: \n");
+            Console.WriteLine("1. Math:         - 1");
+            Console.WriteLine("2. Programming:  - 2");
+            Console.WriteLine("3. Philosophy:   - 3");
+            Console.WriteLine("4. Physics:      - 4");
+            Console.WriteLine("5. English       - 5\n");
+            int key = Convert.ToInt32(Console.ReadLine());
+            return key - 1;
+        }
+
         static void Main(string[] args)
         {
-
-            int[,] arr1, arr2, resArr  = null; // создали две матрицы
-
-            Console.Write("Задайте количество столбцов матрицы: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Задайте количество строк матрицы: ");
-            int m = Convert.ToInt32(Console.ReadLine());
-            arr1 = new int[m, n]; // выделение памяти под первую матрицу
-            arr2 = new int[m, n]; // выделение памяти под первую матрицу
-            resArr = new int[m, n]; // выделение памяти под первую матрицу
-
-            // инициализация элементов матриц
-            Random rand = new Random();
-            for (int i = 0; i < m; i++)
+            const int m = 5, n = 20;           
+            int[,] gradeArr = new int[m, n]; // создали зубчатый массив
+            string[] Themes = new string[] { "Math", "Programming", "Philosophy", "Physics", "English" };           
+            int key = 0;
+            do
             {
-                for(int j=0;j<n; j++)
+                Console.WriteLine("\tВыберите действие");
+                Console.WriteLine("\n1. Выствить оценку по предмету     - 1");
+                Console.WriteLine("2. Средний балл оценок по предмету - 2");
+                Console.WriteLine("3. Средний балл по всем оценкам    - 3");
+                Console.WriteLine("4. Поиск максимальной оценки       - 4");
+                Console.WriteLine("5. Поиск минимальной оценки        - 5");
+                Console.WriteLine("6. Выход                           - 0\n");
+                gradeBill(gradeArr, n);
+                key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
                 {
-                    arr1[i, j] = rand.Next(-99, 99);
-                    arr2[i, j] = rand.Next(-99, 99);
+                    case 1:
+                        gradeIn(gradeArr, themes());
+                        break;
+                    case 2:
+                        int theme = themes();
+                        Console.WriteLine($"Средняя оценка по {Themes[theme]}: " + themeAverage(gradeArr, theme));
+                        Console.WriteLine("Нажмите любую клавишу для продолжения");
+                        Console.ReadKey();
+                        break;
+                    case 3:                      
+                        Console.WriteLine($"Средняя оценка по предметам: " + allAverage(gradeArr));
+                        Console.WriteLine("Нажмите любую клавишу для продолжения");
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.WriteLine($"Максимальная оценка по предметам: " + maxGrade(gradeArr));
+                        Console.WriteLine("Нажмите любую клавишу для продолжения");
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        Console.WriteLine($"Минимальная оценка по предметам: " + maxGrade(gradeArr));
+                        Console.WriteLine("Нажмите любую клавишу для продолжения");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        break;
                 }
+                Console.Clear();
+            } while (key !=0 );
 
-            }
-            //вывод матрицы 1 на экран
-            Console.WriteLine("Матрица_#1: ");
-            printArr(arr1);
-            // вывод матрицы 2 на экран
-            Console.WriteLine("Матрица_#2: ");
-            printArr(arr2);
-
-            for (int i = 0; i < m; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    resArr[i, j] = arr1[i, j] + arr2[i, j];
-                }
-            }
-            Console.WriteLine("Сумма матриц: ");
-            printArr(resArr);
-
-            for (int i = 0; i < m; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    resArr[i, j] = arr1[i, j] * arr2[i, j];
-                }
-            }
-            Console.WriteLine("Произведение матриц: ");
-            printArr(resArr);
-
-            for (int i = 0; i < m; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    resArr[i, j] = arr1[i, j] - arr2[i, j];
-                }
-            }
-            Console.WriteLine("Разность матриц: ");
-            printArr(resArr);
-               
-            Console.WriteLine($"Среднее арифметическое матрицы arr1: {Average(arr1)}");
-            Console.WriteLine($"Среднее арифметическое матрицы arr2: {Average(arr2)}");
-            Console.WriteLine($"Среднее арифметическое матрицы resArr: {Average(resArr)}");                   
+            Console.WriteLine("Успехов в учёбе! ");           
         }
     }
 }
