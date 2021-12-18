355b80bca2dff57ccdffab400680faccf521ecc0
burningborn <evgenyborn@gmail.com>
2021-12-18 23:09:15 +0400

    Tanks

---
+++ Tanks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MyClassLib.WordOfTanks;
+
+namespace Tanks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tanks T34 = new Tank();
+            
+        }
+    }
+}
