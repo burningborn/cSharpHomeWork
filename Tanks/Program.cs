b28a89bf6ecced9ccbcdd619704f7a1f34bedbec
burningborn <evgenyborn@gmail.com>
2022-01-03 14:38:35 +0400

    Tanks

--- Tanks/Program.cs
+++ Tanks/Program.cs
@@ -1,14 +1,48 @@
 ﻿using System;
-using MyClassLib.WordOfTanks;
+//using MyClassLib.WordOfTanks;
+using Tanks;
+
 
 namespace Tanks
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Tanks T34 = new Tank();
-            
+            Tank[] T34 = new Tank[5];        
+                T34[0] = new Tank("T 34.1");
+                T34[1] = new Tank("T 34.2");
+                T34[2] = new Tank("T 34.3");
+                T34[3] = new Tank("T 34.4");
+                T34[4] = new Tank("T 34.5");
+
+            Tank[] Pantera = new Tank[5];
+                Pantera[0] = new Tank("Pantera.1");
+                Pantera[1] = new Tank("Pantera.2");
+                Pantera[2] = new Tank("Pantera.3");
+                Pantera[3] = new Tank("Pantera.4");
+                Pantera[4] = new Tank("Pantera.5");
+
+            for (int i = 0; i < 5; i++)
+                T34[i].Display();
+            for (int i = 0; i < 5; i++)
+                Pantera[i].Display();
+            int T = 0, P = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+            bool Balte = T34[i] ^ Pantera[i];
+                if (Balte)
+                    T++;
+                else
+                    P++;
+            }
+            Console.WriteLine("\n");
+            if(T < P)
+            Console.WriteLine("Winner is Pantera");
+            else
+            Console.WriteLine("Winner is T34");
         }
     }
 }
---
+++ Tanks/Tank.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tanks
+{
+    class Tank
+    {
+       string name;       //название танка
+       int ammunition;    //боекомплект
+       int armor;         //уровень брони
+       int mobility;      //уровень маневренности
+
+        Random rnd = new Random();
+        public Tank()
+        {
+            name = null;
+            ammunition = 0;
+            armor = 0;
+            mobility = 0;
+        }
+        public Tank(string _name)
+        {
+
+            name = _name;
+            ammunition = rnd.Next(0, 100);
+            armor = rnd.Next(0, 100);
+            mobility = rnd.Next(0, 100);
+        }
+
+        public static bool operator ^(Tank tankOne, Tank tankTwo)
+        {
+            int winer = 0;
+
+            if (tankOne.ammunition > tankTwo.ammunition) winer++;
+            if (tankOne.armor > tankTwo.armor) winer++;
+            if (tankOne.mobility > tankTwo.mobility) winer++;
+
+            return winer >= 2;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"\n Name - { name}, Armor - {armor}, Mobility - {mobility}, Ammunition - {ammunition}");
+        }
+
+    }
+}
